c75e900ec0cfa80e73f2f1b22474dff203787237
Your Name <yourgithubemail@gmail.com>
2026-02-22 11:37:05 +0530

    all components

--- project_01/src/comonents/index.jsx
+++ project_01/src/comonents/index.jsx
@@ -1,6 +1,11 @@
-let step1 = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            </div>
-    )
+
+//creating calculator app using react and tailwind css
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Calculator App
+      </h1>
+    </div>
+  );
 }
