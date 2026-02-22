04a13b84ff36d8cc9a1eefdbccb2f24fcf8f09f4
Your Name <yourgithubemail@gmail.com>
2026-02-22 10:35:06 +0530

    name rename

---
+++ project_01/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind v4 is working ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App;
